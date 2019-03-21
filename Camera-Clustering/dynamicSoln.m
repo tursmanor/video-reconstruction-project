ad68881f9d2228fda257d4cb01db50585267d70c
tursmanor <tursmane@grinnell.edu>
2019-03-20 23:53:06 -0400

    sequence cost calculator passes initial batch of tests

--- Camera-Clustering/dynamicSoln.m
+++ Camera-Clustering/dynamicSoln.m
@@ -10,14 +10,14 @@ close all; clearvars;
 %% Load data
 load 'Results/dataset-test2.mat';
 n = length(dataset);
-positions = zeros(n,2);
+positions = zeros(n,6);
 shotLengths = zeros(n,1);
 fs = zeros(n,2);
 numCam = 4;
 
 for i=1:n
     [avgP,avgF] = avgCamera(dataset(i).pos,dataset(i).f);
-    positions(i,:) = avgP(:,2);
+    positions(i,:) = [avgP(:,1)' avgP(:,2)' avgP(:,3)'];
     fs(i,:) = avgF;
     shotLengths(i) = dataset(i).frame;
 end
@@ -82,24 +82,179 @@ function [cost] = calcCost(sequence,positions,shotLengths,fs)
         curCam = sequence(i);
         if (prevCam == curCam)
             cost = -inf;
-            break;
+            return;
+        end
+            
+        % check if cur camera has appeared before, if so, see if its path
+        % intersects the constraint region-- in other words, see if its
+        % path intersects either of the lines defining the constraint
+        [indx] = lastAppearance(i,sequence);
+        
+        if (indx ~= 0)
+            
+            prevPos = positions(indx,:);
+            curPos = positions(i,:);
+            conPos = positions(i-1,:);
+            
+            % set up line intersection as a system of linear equations
+            aVal = @(x1,x2,y1,y2) -(x2 - x1)/(y2 - y1);
+            bVal = @(x1,x2,y1,y2) (-y1 * ((x2-x1)/(y2-y1))) + x1;
+            slope = @(x1,x2,y1,y2) (y2 - y1)/(x2 - x1);
+            
+            aValPath = aVal(prevPos(3),curPos(3),prevPos(4),curPos(4));
+            bValPath = bVal(prevPos(3),curPos(3),prevPos(4),curPos(4));
+            pathSlope = slope(prevPos(3),curPos(3),prevPos(4),curPos(4));
+            
+            aValLeft = aVal(conPos(5),fs(i-1,1),conPos(6),fs(i-1,2));
+            bValLeft = bVal(conPos(5),fs(i-1,1),conPos(6),fs(i-1,2));
+            aValRight = aVal(conPos(1),fs(i-1,1),conPos(2),fs(i-1,2));
+            bValRight = bVal(conPos(1),fs(i-1,1),conPos(2),fs(i-1,2));
+            
+            ALeft =  [1 aValLeft; 1 aValPath];
+            ARight = [1 aValRight; 1 aValPath];
+            bLeft =  [bValLeft; bValPath];
+            bRight = [bValRight; bValPath];
+            
+            leftIntersect = ALeft\bLeft;
+            rightIntersect = ARight\bRight;
+            
+            % check if intersection occurs, otherwise accumulate distance
+            % traveled
+            if (~testIntersectionBounds(leftIntersect, pathSlope, fs(i-1,:), curPos(3:4), prevPos(3:4)) || ...
+                    ~testIntersectionBounds(rightIntersect, pathSlope, fs(i-1,:), curPos(3:4), prevPos(3:4)))
+                cost = -inf;
+                return;
+            else
+                % using d/v = t, where v is one m/s
+                d = norm(prevPos(3:4) - positions(i,3:4));
+                alpha = 0.1;
+                cost = cost + d + (alpha * abs(d - shotLengths(i)));
+            end
+            
         end
         
-        constraintRegion = ;
-        
-        % check if cur camera has appeared before, if so, see if its path
-        % intersects the constraint region
-        
-        
-   
         prevCam = curCam;
+        
     end
 end
 
-function [out] = lastAppearance();
+% returns 0 if the intersection happens in a constraint region, 1 if not
+function [valid] = testIntersectionBounds(pt, pSlope, f, pEnd,pBeg)
+
+% infinity check-- means we have parallel lines
+% bounds check-- only care if intersection occurs in our scene
+if ((pt(2) == inf) || pt(2) > 10)
+    valid = true;
+end
+
+% check if path is going in the pos or neg x direction
+if (pBeg(1) < pEnd(1))
+    right = true;
+else
+    right = false;
+end
+
+% intersection point is above the start of the constraint triangle
+if (pt(2) > f(2))
+    
+    % positive slope
+    if(pSlope > 0)
+        
+        % path is pointing right
+        if(right)
+            
+            % point of intersection is larger in x and y than path end
+            if(pt(1) > pEnd(1) && pt(2) > pEnd(2))
+                valid = true;
+            else
+                valid = false;
+            end
+            
+        % path is pointing left
+        else
+            
+            % point of intersection is smaller in x and y than path end
+            if(pt(1) < pEnd(1) && pt(2) < pEnd(2))
+                valid = true;
+            else
+                valid = false;
+            end
+            
+        end
+        
+    % negative slope
+    elseif (pSlope < 0)
+        
+        % path is pointing right
+        if(right)
+            
+            % point of intersection is larger in x and smaller in y than 
+            % path end
+            if(pt(1) > pEnd(1) && pt(2) < pEnd(2))
+                valid = true;
+            else
+                valid = false;
+            end
+            
+        % path is pointing left
+        else
+            
+            % point of intersection is smaller in x and bigger in y than 
+            % path end
+            if(pt(1) < pEnd(1) && pt(2) > pEnd(2))
+                valid = true;
+            else
+                valid = false;
+            end  
+        end
+            
+    % straight line    
+    else
+        
+        % path is pointing right
+        if(right)
+          
+            % point of intersection larger in x than path end
+            if (pt(1) > pEnd(1))
+                valid = true;
+            else
+                valid = false;
+            end
+            
+        % path is pointing left    
+        else
+            
+            % point of intersection smaller in x than path end
+            if (pt(1) < pEnd(1))
+                valid = true;
+            else
+                valid = false;
+            end     
+        end   
+    end
+    
+else
+    valid = true;
+end
 
 end
 
+% returns most recent instance of camera before curInd, 0 if it doesn't
+% appear before curInd
+function [indx] = lastAppearance(curInd,sequence)
+    
+    indx = 0;
+    cameraId = sequence(curInd);
+    
+    for i=(curInd-1):-1:1
+        if (sequence(i) == cameraId)
+            indx = i;
+        break;
+        end
+    end
+    
+end
+    
 % Calculate table
 %
 % Output: 
@@ -112,105 +267,105 @@ end
 % positions    - nx2, positions associated with each shot
 % shotLengths  - nx1, lengths of each shot
 % shotSubset   - subset of shots
-function [minError,cameraConfig] = dynamicCamAssignment(numCameras,n,shotSubset,positions,shotLengths,fs)
-
-global indexTable errorTable widthsTable;
-
-% Collect an error for each camera num
-widthErrors = zeros(numCameras, 1);
-widthPatterns = cell(numCameras, 1);
-
-for i=1:numCameras
-    
-    camWidth = 1;
-    curCam = i;
-    
-    % First, get remaining subset width after placing
-    % this lens at right-most part of epiSlice
-    upToWidth = shotSubset - camWidth;
-    
-    % If this width is negative, i.e., if the lens width is larger,
-    % then this lens cannot fit and we assign a large error
-    if upToWidth < 0
-        widthErrors(i) = Inf;
-        widthPatterns(i) = {-1};
-        % If the lens width equals the remaining subset width
-    elseif upToWidth == 0
-        % This is a base case.
-        % Compute the error directly.
-        %
-        widthErrors(i) = calcCost(upToWidth, positions(1:size(upToWidth),:),shotLengths(1:size(upToWidth)));
-        widthPatterns(i) = {curCam};
-        
-        % It is possible for a length of 'lensWidth' to already
-        % exist from a deeper level plus a lens (e.g., 10 will
-        % already exist from 5 + 5.
-        index = indexTable == camWidth;
-        if sum( index ) == 0
-            % Doesn't exist in the table; add
-            indexTable = [indexTable camWidth];
-            errorTable = [errorTable widthErrors(i)];
-            widthsTable = [widthsTable widthPatterns(i)];
-        else
-            % Replace existing value if error is less.
-            errors = errorTable;
-            errors(~index) = Inf;
-            [minE,ind] = min(errors);
-            
-            if widthErrors(i) < minE
-                errorTable( ind ) = widthErrors(i);
-                widthsTable( ind ) = widthPatterns(i);
-            end
-        end
-    % Else
-    else
-        % Look up this width in T. If empty, recurse.
-        index = indexTable == upToWidth;
-        % If there's no entry in errorTable for this width
-        if sum( index ) == 0
-            % We need to recurse and find the error
-            [upToError,widths] = dynamicCamAssignment(numCameras,n,upToWidth,positions,shotLengths);
-        else
-            errors = errorTable;
-            errors(~index ) = Inf;
-            [~,ind] = min(errors);
-            upToError = errorTable( ind );
-            widths = widthsTable( ind );
-        end
-        
-        % The combined error for this lensWidth is equal to the
-        % upToError plus the lens error placed at the end.
-        lensError = calcCost(upToWidth, positions(1:size(upToWidth),:),shotLengths(1:size(upToWidth)));
-        
-        % Set error and width pattern that achieves this error.
-        widthErrors(i) = upToError + lensError;
-        widthPatterns(i) = {[widths{:} curCam]};
-        
-        % Add this entry to the index table ONLY if it hasn't been
-        % added before OR if it is the minimum error
-        % (and if so, replace the existing minimum error)
-        newIndex = upToWidth+camWidth;
-        index = indexTable == newIndex;
-        if sum( index ) == 0
-            % Doesn't exist in the table; add
-            indexTable = [indexTable newIndex];
-            errorTable = [errorTable widthErrors(i)];
-            widthsTable = [widthsTable widthPatterns(i)];
-        else
-            % Replace existing value if error is less.
-            errors = errorTable;
-            errors( ~index ) = Inf;
-            [minE,ind] = min(errors);
-            
-            if widthErrors(i) < minE
-                errorTable( ind ) = widthErrors(i);
-                widthsTable( ind ) = widthPatterns(i);
-            end
-        end
-    end
-end
-
-% Find minimum error and return best pattern of widths.
-[minError,index] = min( widthErrors );
-cameraConfig = widthPatterns(index);
-end
+% function [minError,cameraConfig] = dynamicCamAssignment(numCameras,n,shotSubset,positions,shotLengths,fs)
+% 
+% global indexTable errorTable widthsTable;
+% 
+% % Collect an error for each camera num
+% widthErrors = zeros(numCameras, 1);
+% widthPatterns = cell(numCameras, 1);
+% 
+% for i=1:numCameras
+%     
+%     camWidth = 1;
+%     curCam = i;
+%     
+%     % First, get remaining subset width after placing
+%     % this lens at right-most part of epiSlice
+%     upToWidth = shotSubset - camWidth;
+%     
+%     % If this width is negative, i.e., if the lens width is larger,
+%     % then this lens cannot fit and we assign a large error
+%     if upToWidth < 0
+%         widthErrors(i) = Inf;
+%         widthPatterns(i) = {-1};
+%         % If the lens width equals the remaining subset width
+%     elseif upToWidth == 0
+%         % This is a base case.
+%         % Compute the error directly.
+%         %
+%         widthErrors(i) = calcCost(upToWidth, positions(1:size(upToWidth),:),shotLengths(1:size(upToWidth)));
+%         widthPatterns(i) = {curCam};
+%         
+%         % It is possible for a length of 'lensWidth' to already
+%         % exist from a deeper level plus a lens (e.g., 10 will
+%         % already exist from 5 + 5.
+%         index = indexTable == camWidth;
+%         if sum( index ) == 0
+%             % Doesn't exist in the table; add
+%             indexTable = [indexTable camWidth];
+%             errorTable = [errorTable widthErrors(i)];
+%             widthsTable = [widthsTable widthPatterns(i)];
+%         else
+%             % Replace existing value if error is less.
+%             errors = errorTable;
+%             errors(~index) = Inf;
+%             [minE,ind] = min(errors);
+%             
+%             if widthErrors(i) < minE
+%                 errorTable( ind ) = widthErrors(i);
+%                 widthsTable( ind ) = widthPatterns(i);
+%             end
+%         end
+%     % Else
+%     else
+%         % Look up this width in T. If empty, recurse.
+%         index = indexTable == upToWidth;
+%         % If there's no entry in errorTable for this width
+%         if sum( index ) == 0
+%             % We need to recurse and find the error
+%             [upToError,widths] = dynamicCamAssignment(numCameras,n,upToWidth,positions,shotLengths);
+%         else
+%             errors = errorTable;
+%             errors(~index ) = Inf;
+%             [~,ind] = min(errors);
+%             upToError = errorTable( ind );
+%             widths = widthsTable( ind );
+%         end
+%         
+%         % The combined error for this lensWidth is equal to the
+%         % upToError plus the lens error placed at the end.
+%         lensError = calcCost(upToWidth, positions(1:size(upToWidth),:),shotLengths(1:size(upToWidth)));
+%         
+%         % Set error and width pattern that achieves this error.
+%         widthErrors(i) = upToError + lensError;
+%         widthPatterns(i) = {[widths{:} curCam]};
+%         
+%         % Add this entry to the index table ONLY if it hasn't been
+%         % added before OR if it is the minimum error
+%         % (and if so, replace the existing minimum error)
+%         newIndex = upToWidth+camWidth;
+%         index = indexTable == newIndex;
+%         if sum( index ) == 0
+%             % Doesn't exist in the table; add
+%             indexTable = [indexTable newIndex];
+%             errorTable = [errorTable widthErrors(i)];
+%             widthsTable = [widthsTable widthPatterns(i)];
+%         else
+%             % Replace existing value if error is less.
+%             errors = errorTable;
+%             errors( ~index ) = Inf;
+%             [minE,ind] = min(errors);
+%             
+%             if widthErrors(i) < minE
+%                 errorTable( ind ) = widthErrors(i);
+%                 widthsTable( ind ) = widthPatterns(i);
+%             end
+%         end
+%     end
+% end
+% 
+% % Find minimum error and return best pattern of widths.
+% [minError,index] = min( widthErrors );
+% cameraConfig = widthPatterns(index);
+% end
